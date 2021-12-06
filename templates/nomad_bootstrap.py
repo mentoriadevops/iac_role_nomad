c6a5d4ab483fd86a31032894419ce9397ae60620
Marcos Rezende <rehzende@hotmail.com>
2021-12-06 12:59:23 -0300

    refactor: ajusta o padrão do nomad_path e corrige o help

--- templates/nomad_bootstrap.py
+++ templates/nomad_bootstrap.py
@@ -6,18 +6,18 @@ from string import Template
 
 def server(bootstrap_expect, retry_join, nomad_path):
     retry_join =  f'"{retry_join}"'
-    render_config(f"{nomad_path}/server.hcl.tpl", "<BOOTSTRAP_EXPECT>", bootstrap_expect)
-    render_config(f"{nomad_path}/server.hcl.tpl", "<RETRY_JOIN>", retry_join)
+    render_config(f"{nomad_path}server.hcl.tpl", "<BOOTSTRAP_EXPECT>", bootstrap_expect)
+    render_config(f"{nomad_path}server.hcl.tpl", "<RETRY_JOIN>", retry_join)
         
 def client(retry_join, nomad_path):
     retry_join =  f'"{retry_join}"'
-    render_config(f"{nomad_path}/client.hcl.tpl", "<RETRY_JOIN>", retry_join)
+    render_config(f"{nomad_path}client.hcl.tpl", "<RETRY_JOIN>", retry_join)
 
 def both(bootstrap_expect, retry_join, nomad_path):
     retry_join = f'"{retry_join}"'
-    render_config(f"{nomad_path}/server.hcl.tpl", "<BOOTSTRAP_EXPECT>", bootstrap_expect)
-    render_config(f"{nomad_path}/server.hcl.tpl", "<RETRY_JOIN>", retry_join)
-    render_config(f"{nomad_path}/client.hcl.tpl", "<RETRY_JOIN>", retry_join)
+    render_config(f"{nomad_path}server.hcl.tpl", "<BOOTSTRAP_EXPECT>", bootstrap_expect)
+    render_config(f"{nomad_path}server.hcl.tpl", "<RETRY_JOIN>", retry_join)
+    render_config(f"{nomad_path}client.hcl.tpl", "<RETRY_JOIN>", retry_join)
 
 def render_config(file, searchExp, replaceExp):
    for line in fileinput.input(file, inplace=1):
@@ -38,14 +38,14 @@ if __name__ == '__main__':
                                                 nomad_boostrap.py both -b 1
                                                 
                                             Iniciar cluster no GCP com cloud auto-join:
-                                                nomad_boostrap.py server  -b 3 -r "provider=gce project_name=meu-projeto tag_value=nomad-server"
-                                                nomad_boostrap.py client -r "provider=gce project_name=meu-projeto tag_value=nomad-server"
+                                                nomad_bootstrap.py server  -b 3 -r "provider=gce project_name=meu-projeto tag_value=nomad-server"
+                                                nomad_bootstrap.py client -r "provider=gce project_name=meu-projeto tag_value=nomad-server"
                                         """))
     
     parser.add_argument('mode', help='Modo de execução do nomad', choices=['server', 'client', 'both'])
     parser.add_argument('--bootstrap-expect', '-b', help='Número de nodes do servidor a aguardar antes de inicializar.', type=int)
     parser.add_argument('--retry-join', '-r', help='', type=str, default='127.0.0.1')
-    parser.add_argument('--nomad_path', help='Caminho do nomad', type=str, default='/etc/nomad.d')
+    parser.add_argument('--nomad_path', help='Caminho do nomad', type=str, default='/etc/nomad.d/')
     args = parser.parse_args()
 
     if not validate_args(args):
